0e8da11d6a50f6e54303895a214939fbdb74eb0b
Khuram Hafeez <khuramhaf@gmail.com>
2022-12-16 22:51:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './component/About';
+import {useState} from 'react'
+import Home from './component/Home';
+import Services from './component/Services';
+import Contact from './component/Contact';
+import {Routes, Route, Link, useNavigate} from 'react-router-dom';
 
 function App() {
+
+  const  navigate=useNavigate();
+const [state, setstate] = useState(false)
+  function myfun1(){
+setstate(1)
+setTimeout(()=>{
+  setstate(false)
+  navigate('/')
+  
+}, 1000)
+  }
+
+function myfun2(){
+  setstate(2)
+
+  setTimeout(()=>{
+    setstate(false)
+    navigate('/about')
+    
+  }, 1000)
+}
+
+function myfun3(){
+  setstate(3)
+  setTimeout(()=>{
+    setstate(false)
+    navigate('/services')
+   
+  }, 1000)}
+
+  function myfun4(){
+    setstate(4)
+    setTimeout(()=>{
+      setstate(false)
+      navigate('/contact')
+      
+    }, 1000)}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <button onClick={myfun1}>clickme</button>
+      <button onClick={myfun2}>clickme</button>
+      <button onClick={myfun3}>clickme</button>
+      <button onClick={myfun4}>clickme</button>
+
+      <Routes>
+        <Route path="/" element={<Home state={state}/>}></Route>
+        <Route path="/about" element={<About state={state}/>}></Route>
+        <Route path="/services" element={<Services state={state}/>}></Route>
+        <Route path="/contact" element={<Contact state={state}/>}></Route>
+      </Routes>
+  
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,191 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+import '../App.css'
+
+
+
+function Home(props) {
+
+    const style1 = {
+        marginLeft: "-550px",
+       
+    }
+
+    const style2 = {
+          marginLeft: "500px", 
+        top:"44px",
+        position:"absolute"
+    }
+
+    const style3 = {
+        marginLeft: "1366px",
+        transition: 'margin-left 1s'
+    }
+
+    const style5 = {
+        marginLeft: "-550px",
+        position:'absolute',
+        top:'44px'
+    }
+
+
+    const style4 = {
+        marginLeft: "500px", 
+        transition: 'margin-left 1s',
+        position:'absolute',
+        top:'44px'
+        
+      
+    }
+
+    const [homestate, sethomestate] = useState(style2)
+    const [aboutstate, setaboutstate] = useState(style5)
+    const [dsh, sdsh] = useState("hidden")
+    const [dsa, sdsa] = useState("hidden")
+    const [dss, sdss] = useState("hidden")
+    const [dsc, sdsc] = useState("hidden")
+
+
+    useEffect(()=>{
+
+        if(homestate===style1){
+
+            sethomestate(style2)
+        }
+
+
+       
+
+      
+    })
+
+
+    useEffect(()=>{
+
+       if(props.state !== false){
+
+
+
+        if (props.state===1&& window.location.pathname!=="/"){
+
+            sethomestate(style3)
+            
+            setaboutstate(style4)
+
+            sdsh("visible")
+        }
+
+        if (props.state===2&& window.location.pathname!=="/about"){
+
+            sethomestate(style3)
+            
+            setaboutstate(style4)
+
+            sdsa("visible")
+        }
+
+
+        if (props.state===3 && window.location.pathname!=="/services"){
+
+            sethomestate(style3)
+            
+            setaboutstate(style4)
+
+            sdss("visible")
+        }
+
+
+        if (props.state===4&& window.location.pathname!=="/contact"){
+
+            sethomestate(style3)
+            
+            setaboutstate(style4)
+
+            sdsc("visible")
+        }
+}
+
+
+
+
+    }, [props.state])
+
+
+    return ( 
+
+        <div>
+            
+<div id="sticky-container"  style={homestate}>
+<h1>Home</h1>
+<p className='homep'>
+
+We are team of two talented developers with an aim to develop web user interfaces for future. We keep our eye on the evolving nature of the web development. We see frontend development specially single page app is the technology of the future and we can achieve many special things which were not possible before.
+<br></br>
+Contact us @ contact@musk-technology.com
+
+</p>
+
+</div>
+
+
+<div style={{visibility: dsh}}>
+<div style={aboutstate}>
+
+<h1>Home</h1>
+<p className='homep'>
+
+We are team of two talented developers with an aim to develop web user interfaces for future. We keep our eye on the evolving nature of the web development. We see frontend development specially single page app is the technology of the future and we can achieve many special things which were not possible before.
+<br></br>
+Contact us @ contact@musk-technology.com
+
+</p>
+</div>
+</div>
+
+<div style={{visibility: dsa}}>
+<div style={aboutstate}>
+
+<h1>About</h1>
+<p className='homep'>
+
+We are team of two talented developers with an aim to develop web user interfaces for future. We keep our eye on the evolving nature of the web development. We see frontend development specially single page app is the technology of the future and we can achieve many special things which were not possible before.
+<br></br>
+Contact us @ contact@musk-technology.com
+
+</p>
+</div>
+</div>
+
+<div style={{visibility: dss}}>
+<div  style={aboutstate}>
+
+<h1>Services</h1>
+<p className='homep'>
+
+We are team of two talented developers with an aim to develop web user interfaces for future. We keep our eye on the evolving nature of the web development. We see frontend development specially single page app is the technology of the future and we can achieve many special things which were not possible before.
+<br></br>
+Contact us @ contact@musk-technology.com
+
+</p>
+</div>
+</div>
+
+
+<div style={{visibility: dsc}}>
+<div style={aboutstate}>
+
+<h1>Contact us</h1>
+<p className='homep'>
+
+We are team of two talented developers with an aim to develop web user interfaces for future. We keep our eye on the evolving nature of the web development. We see frontend development specially single page app is the technology of the future and we can achieve many special things which were not possible before.
+<br></br>
+Contact us @ contact@musk-technology.com
+
+</p>
+</div>
+</div>
+</div>
+     );
+}
+
+export default Home;
